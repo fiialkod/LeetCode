9799202e6c8efefed3793d5a37916512ca2546c0
Denys Fiialko <denysfiialko@MacBook-Pro.local>
2022-01-02 23:10:41 -0800

    Simple approach Task 973

---
+++ LeetCode/KClosest_973.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+	public class KClosest_973
+	{
+		public KClosest_973()
+		{
+		}
+	}
+}
+
---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LeetCode
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
